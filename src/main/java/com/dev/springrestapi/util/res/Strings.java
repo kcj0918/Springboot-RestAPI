0aa2931d24459b6689634a9dadfc51a81fe3ab10
CheolJin Kang <kcj091857@gmail.com>
2020-10-07 17:27:55 +0900

    Create Swagger Configuration File and gradle Configuration and H2 Configuration

---
+++ src/main/java/com/dev/springrestapi/util/res/Strings.java
@@ -0,0 +1,6 @@
+package com.dev.springrestapi.util.res;
+
+public class Strings {
+    public static final String CAN_NOT_FOUND_USER = "Can not found user.";
+    public static final String ALREADY_EXIST_EMAIL = "Already exist email.";
+}
