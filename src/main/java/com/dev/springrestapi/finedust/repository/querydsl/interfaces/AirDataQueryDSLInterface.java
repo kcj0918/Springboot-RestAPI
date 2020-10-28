edb5f1e9364f4af6c742d5f5750bada27e86fef7
CheolJin Kang <kcj091857@gmail.com>
2020-10-28 18:31:08 +0900

    Update finedust querydsl interface
    Update AirDataQueryDSLInterface add method getAirDataByStation

--- src/main/java/com/dev/springrestapi/finedust/repository/querydsl/interfaces/AirDataQueryDSLInterface.java
+++ src/main/java/com/dev/springrestapi/finedust/repository/querydsl/interfaces/AirDataQueryDSLInterface.java
@@ -1,4 +1,7 @@
 package com.dev.springrestapi.finedust.repository.querydsl.interfaces;
 
+import com.dev.springrestapi.finedust.domain.AirData;
+
 public interface AirDataQueryDSLInterface {
+    AirData getAirDataByStationName(String beginDate, String endDate, String stationName);
 }
