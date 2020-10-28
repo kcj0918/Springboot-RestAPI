a89a03936f98aacd0820431442228c5b94f6af16
CheolJin Kang <kcj091857@gmail.com>
2020-10-28 18:24:30 +0900

    Update AirDataRepository
    Update AirDataRepository

--- src/main/java/com/dev/springrestapi/finedust/repository/AirDataRepository.java
+++ src/main/java/com/dev/springrestapi/finedust/repository/AirDataRepository.java
@@ -1,4 +1,10 @@
 package com.dev.springrestapi.finedust.repository;
 
-public class AirDataRepository {
+import com.dev.springrestapi.finedust.domain.AirData;
+import com.dev.springrestapi.finedust.repository.querydsl.interfaces.AirDataQueryDSLInterface;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AirDataRepository extends JpaRepository<AirData, Long>, AirDataQueryDSLInterface {
 }
