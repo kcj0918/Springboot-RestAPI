4c36fdbb62455a90cfb5b29600b9cc47c2c61170
CheolJin Kang <kcj091857@gmail.com>
2020-10-28 18:36:48 +0900

    Modify finedust querydsl file
    Modify AirDataQueryDSLImpl, AirDataQueryDSLInterface getAirDataByStationName function

--- src/main/java/com/dev/springrestapi/finedust/repository/querydsl/AirDataQueryDSLImpl.java
+++ src/main/java/com/dev/springrestapi/finedust/repository/querydsl/AirDataQueryDSLImpl.java
@@ -1,4 +1,23 @@
 package com.dev.springrestapi.finedust.repository.querydsl;
 
-public class AirDataQueryDSLImpl {
+import com.dev.springrestapi.finedust.domain.AirData;
+import com.dev.springrestapi.finedust.domain.QAirData;
+import com.dev.springrestapi.finedust.repository.querydsl.interfaces.AirDataQueryDSLInterface;
+import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class AirDataQueryDSLImpl extends QuerydslRepositorySupport implements AirDataQueryDSLInterface {
+    private QAirData qAirData = QAirData.airData;
+
+    public AirDataQueryDSLImpl() {
+        super(AirData.class);
+    }
+
+    @Override
+    public List<AirData> getAirDataByStationName(String beginDate, String endDate, String stationName) {
+        return null;
+    }
 }
--- src/main/java/com/dev/springrestapi/finedust/repository/querydsl/interfaces/AirDataQueryDSLInterface.java
+++ src/main/java/com/dev/springrestapi/finedust/repository/querydsl/interfaces/AirDataQueryDSLInterface.java
@@ -2,6 +2,8 @@ package com.dev.springrestapi.finedust.repository.querydsl.interfaces;
 
 import com.dev.springrestapi.finedust.domain.AirData;
 
+import java.util.List;
+
 public interface AirDataQueryDSLInterface {
-    AirData getAirDataByStationName(String beginDate, String endDate, String stationName);
+    List<AirData> getAirDataByStationName(String beginDate, String endDate, String stationName);
 }
